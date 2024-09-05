63c7b8fe4b8ce742d442ef65aa975f841f818b99
christhopermarcelino <christhopermarcelino@gmail.com>
2024-09-05 11:29:19 +0700

    add case

--- explore-marcel.js
+++ explore-marcel.js
@@ -1,22 +1,89 @@
+// SPLICE
 const days = ["Monday", "Wednesday", "Thursday", "Friday"];
-days.splice(1, 0, "Tuesday"); // inserts di index 1
-console.log("days -> ", days);
+days.splice(1, 0, "Tuesday");
+console.log("days after insert -> ", days);
 
+// SLICE
 const languages = ["js", "go", "php", "rust", "python"];
 console.log(languages.slice(1));
 console.log(languages.slice(2, 4));
 console.log(languages.slice(-3));
+console.log(languages.slice(-1));
 
+// BUILT-IN FUNCTIONS
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const resultMap = arr.map((e) => e * 3);
+const resultFilter = arr.filter((e) => e % 2 == 0);
+const resultReduce = arr.reduce((acc, cur) => cur + acc, 0);
+const resultFind = arr.find((e) => e > 6);
+const resultFindIndex = arr.findIndex((e) => e > 6);
+console.log("resultMap -> ", resultMap);
+console.log("resultFilter -> ", resultFilter);
+console.log("resultReduce -> ", resultReduce);
+console.log("resultFind -> ", resultFind);
+console.log("resultFindIndex -> ", resultFindIndex);
 
-const res1 = arr.map((e) => e * 3);
-const res2 = arr.filter((e) => e % 2 == 0);
-const res3 = arr.reduce((acc, cur) => cur + acc, 0);
-const res4 = arr.find((e) => e > 6);
-const res5 = arr.findIndex((e) => e > 6);
+// FILTER
+const words = ["apple", "banana", "grape", "kiwi", "watermelon"];
+const wordsLongerThanFiveCharacters = words.filter((w) => w.length > 5);
+console.log("wordsLongerThanFiveCharacters -> ", wordsLongerThanFiveCharacters);
 
-console.log("res1 -> ", res1);
-console.log("res2 -> ", res2);
-console.log("res3 -> ", res3);
-console.log("res4 -> ", res4);
-console.log("res5 -> ", res5);
+// MAP
+// return full name
+const people = [
+  { firstName: "John", lastName: "Doe" },
+  { firstName: "Jane", lastName: "Smith" },
+];
+const fullNames = people.map((p) => `${p.firstName} ${p.lastName}`);
+console.log("fullNames -> ", fullNames);
+
+// SORT
+// simple sort
+const numbers = [3, 4, 5, 2, 1];
+numbers.sort();
+console.log("simple sort -> ", numbers);
+// sort by value inside object
+const cities = [
+  {
+    country: "surabaya",
+    score: 93,
+  },
+  {
+    country: "jakarta",
+    score: 95,
+  },
+
+  {
+    country: "bandung",
+    score: 90,
+  },
+  {
+    country: "semarang",
+    score: 91,
+  },
+];
+cities.sort((prev, next) => prev.score - next.score);
+console.log("cities after ascending sort -> ", cities);
+cities.sort((prev, next) => next.score - prev.score);
+console.log("cities after descending sort -> ", cities);
+
+const totalScore = cities.reduce((acc, cur) => cur.score + acc, 0);
+console.log("totalScore -> ", totalScore);
+
+// calculate total occurence
+const fruits = ["Banana", "Orange", "Apple", "Orange", "Pear", "Banana"];
+const totalOccurence = fruits.reduce(
+  (acc, cur) => ({ ...acc, [cur]: (acc[cur] ?? 0) + 1 }),
+  {}
+);
+console.log("totalOccurence -> ", totalOccurence);
+
+const me = {
+  firstName: "Mar",
+  lastName: "cel",
+  home: "Jakarta",
+};
+const { firstName, lastName, home } = me;
+console.log("firstName -> ", firstName);
+console.log("lastName -> ", lastName);
+console.log("home -> ", home);
